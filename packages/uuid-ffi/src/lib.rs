8922a29e21478ec915f7d6020e9dbb8c252992c9
hhglory <hhglory@outlook.com>
2024-09-27 09:26:49 +0800

    update

--- packages/uuid-ffi/src/lib.rs
+++ packages/uuid-ffi/src/lib.rs
@@ -1,16 +1,3 @@
 pub mod ffi;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
