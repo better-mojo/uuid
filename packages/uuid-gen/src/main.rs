c70ff966e09574966943ebad63dd03f46a4caccb
hhglory <hhglory@outlook.com>
2024-10-31 12:46:55 +0800

    update

--- packages/uuid-gen/src/main.rs
+++ packages/uuid-gen/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+use diplomat_tool::DocsUrlGenerator;
+
+pub fn main() {
+    //
+    // real gen:
+    //
+    diplomat_tool::gen(
+        Path::new("../uuid-ffi/core/src/lib.rs"),
+        "mojo",
+        Path::new("./build/abi"),
+        &DocsUrlGenerator::default(),
+        None,
+        false,
+    )
+        .unwrap();
+
+
+    //
+    // temp gen: (for double check!)
+    //
+    diplomat_tool::gen(
+        Path::new("../uuid-ffi/core/src/lib.rs"),
+        "mojo",
+        Path::new("../uuid-mojo/src/uuid_mojo/abi/"),
+        &DocsUrlGenerator::default(),
+        None,
+        false,
+    )
+        .unwrap();
 }
